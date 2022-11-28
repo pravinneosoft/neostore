ce379cfa10cefa222d587f470587864890fc0468
pravin satpute <pravin96satpute@gmail.com>
2022-11-28 19:32:35 +0530

    fta22

--- app/js/main.js
+++ app/js/main.js
@@ -39,7 +39,7 @@ $('.owl-carousel').owlCarousel({
 autoplayTimeout:3500 ,
   loop: true,
   items: 1,
-  dot: true,
+  dots: false,
   margin: 10,
   nav: true,});
 function toggleVideo({ currentTarget: wrapper }) {
@@ -54,3 +54,33 @@ function toggleVideo({ currentTarget: wrapper }) {
   }
 }
 // Owl carousel Js end
+
+//countdown timer
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+   
+  // seconds=document.style.color="red";
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
