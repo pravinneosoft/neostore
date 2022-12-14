e77eb839931177ad77e6505e7764604146604681
pravin satpute <pravin96satpute@gmail.com>
2022-12-14 19:21:31 +0530

    ffi

--- app/js/product-detail.js
+++ app/js/product-detail.js
@@ -51,14 +51,57 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+// vertical swiper slider js
+const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { 
+	direction: 'vertical', 
+	slidesPerView: 3, 
+	spaceBetween: 24, // расстояние между слайдами
+	navigation: { // задаем кнопки навигации
+		nextEl: '.slider__next', // кнопка Next
+		prevEl: '.slider__prev' // кнопка Prev
+	},
+	freeMode: true, // при перетаскивании превью ведет себя как при скролле
+	breakpoints: { // условия для разных размеров окна браузера
+		0: { // при 0px и выше
+			direction: 'horizontal', // горизонтальная прокрутка
+		},
+		768: { // при 768px и выше
+			direction: 'vertical', // вертикальная прокрутка
+		}
+	}
+});
+// Инициализация слайдера изображений
+const sliderImages = new Swiper('.slider__images .swiper-container', { // ищем слайдер превью по селектору
+	// задаем параметры
+	direction: 'vertical', // вертикальная прокрутка
+	slidesPerView: 1, // показывать по 1 изображению
+	spaceBetween: 32, // расстояние между слайдами
+	mousewheel: true, // можно прокручивать изображения колёсиком мыши
+	navigation: { // задаем кнопки навигации
+		nextEl: '.slider__next', // кнопка Next
+		prevEl: '.slider__prev' // кнопка Prev
+	},
+	grabCursor: true, // менять иконку курсора
+	thumbs: { // указываем на превью слайдер
+		swiper: sliderThumbs // указываем имя превью слайдера
+	},
+	breakpoints: { // условия для разных размеров окна браузера
+		0: { // при 0px и выше
+			direction: 'horizontal', // горизонтальная прокрутка
+		},
+		768: { // при 768px и выше
+			direction: 'vertical', // вертикальная прокрутка
+		}
+	}
+});
+// vertical swiper slider js end
 
 
 
 
 
 
-
-
+// image zoom js 
 let zoomer = function (){
   document.querySelector('#img-zoomer-box')
     .addEventListener('mousemove', function(e) {
@@ -72,6 +115,9 @@ let zoomer = function (){
         imgHeight = original.offsetHeight,
         xperc = ((x/imgWidth) * 100),
         yperc = ((y/imgHeight) * 100);
+
+        console.log('xperc',xperc);
+        console.log('yperc',yperc);
 
     //lets user scroll past right edge of image
     if(x > (.01 * imgWidth)) {
